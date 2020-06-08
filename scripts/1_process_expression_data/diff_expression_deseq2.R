c973677aae3b4cbe069f2c027dea44eb8e9ba3f8
balazsbohar <bbazsi41@gmail.com>
2020-06-08 16:26:48 +0200

    [balazsbohar] Finished with step 1

---
+++ scripts/1_process_expression_data/diff_expression_deseq2.R
@@ -0,0 +1,112 @@
+# Script to carry out differential expression using Deseq2 with a counts table input. Will only carry out
+# 1 pairwise comparison in 1 run.
+#
+# Author: Agatha Treveil
+# Date: April 2020
+#s
+# Input: Raw counts table with all samples as columns and genes as rows (tab delimited).
+#        Metadata table with conversion between sample IDs and condition names (see 'empty_mapping_table.txt)
+#           - The table should contain one row per input sample
+#           - "sample_name" column should contain the sample name matching the relevant column header in the counts table.
+#           - "condition" column should contain "test" or "control"  depending on whether the sample is a test or control sample (all other values will be ignored). 
+#           - Differential expression is calculated by comparing "test" samples to "control" samples.
+#        lfc cut off and p adj value cut off
+# Output: R deseq2 data object following differential expression
+#        Table of differential expression results - unfiltered
+#        Table of differential expression results - filtered
+# 
+
+####### Set up ######
+
+# Install required packages
+if (!requireNamespace("BiocManager", quietly = TRUE)) 
+  install.packages("BiocManager")
+if (!requireNamespace("DESeq2", quietly = TRUE)) 
+  BiocManager::install("DESeq2")
+if (!requireNamespace("dplyr", quietly = TRUE)) 
+  install.packages("dplyr")
+if (!requireNamespace("tibble", quietly = TRUE)) 
+  install.packages("tibble")
+
+# Load required packages
+library(dplyr)
+library(DESeq2)
+library(tibble)
+
+# Define parameters
+args <- commandArgs(trailingOnly = TRUE)
+
+# Raw counts table
+#counts <- read.csv("/home/balazsbohar/projects/COVID19/covid_stream1/input_files/example_expression_data/GSE147507_RNA_Seq_Raw_Counts.tsv", sep = "\t")
+counts <- read.csv(args[1], sep = "\t")
+# Metadata table
+#meta <- read.csv("/home/balazsbohar/projects/COVID19/covid_stream1/input_files/example_expression_data/GSE147507_mapping.txt", sep = "\t")
+meta <- read.csv(args[2], sep = "\t")
+# Output directory
+#outdir <- "/home/balazsbohar/projects/COVID19/covid_stream1/test/"
+outdir <- args[3]
+# LFC cut off (>=)
+lfccutoff <- 0.5
+
+# Q val cut off (<=)
+pcutoff <- 0.05
+
+####### Pre-processing ######
+
+# Condition names for test and control (as in metadata table)
+test <- "test"
+control <- "control"
+
+# Get vectors of control and test samples IDs
+meta_f <- meta %>% filter((condition == test )| (condition == control))
+
+# Filter counts table to only include the required samples
+counts_f <- counts %>% select(one_of(meta_f$sample_name))
+
+# Convert counts to matrix and add gene names
+counts_fm <- as.matrix(counts_f)
+rownames(counts_fm) <- counts$X
+
+# Test if control is alphabetically after test (weird deseq2 thing)
+#if (test > control){
+#  warning("The control is alphabetically before test condition so the differential expression comparison will be carried out backwards.")
+#}
+
+##### Differential expression ######
+
+# Create deseq2 object
+dds = DESeqDataSetFromMatrix(countData = counts_fm, colData = meta_f, design = ~ condition)
+
+# Set control
+dds$condition <- relevel(dds$condition, ref = control)
+
+# Carry out diff exp
+dds <- DESeq(dds)
+
+##### Save output ######
+
+# Create output dir
+dir.create(file.path(outdir, "1_process_expression_results"), showWarnings = FALSE)
+
+# See the comparisons carried out
+comparison_name <- resultsNames(dds)
+
+# Save the object
+save(dds, file=file.path(outdir, "1_process_expression_results", file="deseq2_dds.Rdata"))
+
+# Get results table
+results <- results(dds, name=comparison_name[2])
+
+# Extract normalized counts data
+dds <- estimateSizeFactors(dds)
+norm_counts <- counts(dds, normalized = TRUE)
+write.table(norm_counts, file.path(outdir, "1_process_expression_results",file="counts_normalised_deseq2.txt"), sep = "\t", quote=F)
+
+# Save differential expression results
+out_file <- paste0("deseq2_res_", comparison_name[2], ".csv")
+write.csv(as.data.frame(results), file=file.path(outdir, "1_process_expression_results", file= out_file))
+
+# Filter results
+results_filt <- as.data.frame(results) %>% tibble::rownames_to_column('Gene') %>% filter((padj <= pcutoff)&((log2FoldChange >= lfccutoff)|(log2FoldChange <= -lfccutoff)))
+out_file2 <- paste0("deseq2_res_", comparison_name[2], "_filtered.csv")
+write.csv(results_filt, file=file.path(outdir, "1_process_expression_results", file=out_file2), row.names = FALSE)
---
+++ scripts/1_process_expression_data/filter_expression_gaussian.py
@@ -0,0 +1,103 @@
+# Script to filter a normalised counts table only for genes which are expressed
+#
+# Input: Normalised counts matrix with genes as rows and samples as columns, tab delimited. First column contains gene
+#        names/ids, all other columns are counts values.
+#
+# Output: Tab delimited text file containing all genes deemed expressed with columns: "Gene", "mean_expression",
+#         "log2_mean_expression" and "zscore".
+
+# Packages
+from scipy.stats import gaussian_kde
+import argparse
+import numpy as np
+import math
+import sys
+import os
+
+
+def parse_args(args):
+    help_text = \
+        """
+        === Filter Expression Gaussian ===
+        
+        Script to filter a normalised counts table only for genes which are expressed.
+        """
+
+    parser = argparse.ArgumentParser(description=help_text)
+
+    parser.add_argument("-i", "--input-file",
+                        help="<path to the data file> [mandatory]",
+                        type=str,
+                        dest="input_file",
+                        action="store",
+                        required=True)
+
+    parser.add_argument("-o", "--output-folder",
+                        help="<path to the output folder> [mandatory]",
+                        type=str,
+                        dest="output_folder",
+                        action="store",
+                        required=True)
+
+    results = parser.parse_args(args)
+    return results.input_file, results.output_folder
+
+
+def main():
+
+    input_file, output_folder = parse_args(sys.argv[1:])
+
+    with open(input_file) as value_list:
+        value_list.readline() # Skip first line / header
+        log_mean = [] # generated here
+        gene_names = []
+        expression_value = [] # raw from input table
+        for line in value_list:
+            line = line.strip().split("\t")
+            mean_ex = sum(map(float,line[1:]))/(len(line) - 1) # Calculate mean of all count values
+            #print(mean_ex)
+
+            if mean_ex == 0:
+                continue
+
+            #print(type(mean_ex))
+            log_val = math.log(mean_ex+1,2) # log2
+            log_mean.append(log_val)
+            expression_value.append(mean_ex)
+            gene_names.append(line[0])
+
+    log_mean = np.array(log_mean)
+
+    # Creating the Gauss-curve,
+    kernel = gaussian_kde(log_mean)
+
+    # Creating X axis -> divide the list for 100 units, xi numpy lists contains that 100 values --> expected value = most oftest value, middle of Gaus-curve
+    xi = np.linspace(log_mean.min(), log_mean.max(), 100)
+
+    # Calculate y for each x point
+    yi = kernel.evaluate(xi)
+
+    # Expected value calculation, which x is by the max y value? (np.argmax(yi) = position)
+    mu = xi[np.argmax(yi)]
+
+    # log_mean > mu  = list of boolean values; mean of values right from the expected value
+    U = log_mean[log_mean > mu].mean()
+
+    #calculation of standard deviation
+    sigma = (U - mu) * np.sqrt(np.pi / 2)
+
+    #new score: deviancy from the mean divided by sigma (standard deviation)
+    #z-value: relative value: deviation from the mean in the st.dev in the data ---> Gaus-curve:  0.001 - 3 deviation (sigma)
+    zscore= (log_mean - mu) / sigma
+
+    # Save results
+    outputname = os.path.join(output_folder, '1_process_expression_results', 'expressed_genes.txt')
+    with open(outputname, 'w') as output_file:
+        output_file.write('Gene' + "\t" + 'mean_expression' + "\t" + "Log2_mean_expression" + "\t" + "zscore" + "\n")
+        result = np.where(zscore > -3)
+        for i in np.nditer(result):
+            output_file.write(str(gene_names[i]) + '\t' + str(expression_value[i]) + '\t' + str(log_mean[i]) + "\t" + str(zscore[i]) + "\n")
+
+
+if __name__ == "__main__":
+    main()

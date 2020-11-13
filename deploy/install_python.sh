734af285e07f10442f60060efea180150b695ed4
agathat <agatha.treveil@gmail.com>
2020-11-13 18:46:57 +0000

    [agatha] Added versions to python packages

--- deploy/install_python.sh
+++ deploy/install_python.sh
@@ -5,10 +5,10 @@ apt-get update
 apt-get -y install python3.6 python3.6-dev python3-setuptools python3-pip
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
-pip3 install --upgrade numpy
-pip3 install --upgrade scipy
-pip3 install --upgrade pandas
-pip3 install --upgrade matplotlib
+pip3 install --upgrade numpy==1.19.4
+pip3 install --upgrade scipy==1.5.4
+pip3 install --upgrade pandas==1.1.4
+pip3 install --upgrade matplotlib==3.3.3
 pip3 install --upgrade argparse
-pip3 install --upgrade pyfasta
-pip3 install --upgrade networkx
+pip3 install --upgrade pyfasta==0.5.2
+pip3 install --upgrade networkx==2.5
